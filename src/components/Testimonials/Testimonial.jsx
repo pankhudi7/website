5786a4b79a3a6f87a5e9dc27a091ae6c95a2cd1b
pankhudi7 <160997950+pankhudi7@users.noreply.github.com>
2024-05-06 11:11:14 +0530

    Update Testimonial.jsx final

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import styles from './testimonial.module.css'; 
 import hero from "../../resources/HeroLogo.png";
-import bajaj from "../../resources/Bajajlogo.png"
+import bajaj from "../../resources/Bajajlogo.png";
+import plustech from "../../resources/PlustechLogo.jpeg";
+import sswl from "../../resources/sswl.jpg"
 
 const Testimonial = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const data = [
     {
-      text: 'Working with Technavia has been a game-changer for our manufacturing process. Their robotic services have revolutionized our production line, increasing efficiency and accuracy beyond our expectations. They are professional,knowledgeable, and dedicated to delivering exceptional results.',
+      text: 'We are proud of You and Your team for everything you have done to convert "An Impossible to a Possible."',
+      image: bajaj,
+      
+    },
+    {
+      text: 'Working with Technavia has been a game-changer for our manufacturing process. Their robotic services have revolutionized our production line, increasing efficiency and accuracy beyond our expectations.',
       image: hero,
     },
     {
-      text: 'We are proud of You and Your team for everything you have done to convert "An Impossible to a Possible."',
-      image: bajaj,
+      text: 'We greatly appreciate the adherence to the main equipment delivery schedule and the swift resolution of identified issues.',
+      image: plustech,
     },
     {
-      text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: 'image3.jpg',
+      text: "Technavia's service is a cornerstone of excellence, boasting knowledgeable engineers who consistently deliver exceptional results.",
+      image: sswl,
     },
   ];
 
@@ -32,16 +39,16 @@ const Testimonial = () => {
     <>
     <div className={styles.tcontainer}>
     <div className={styles.line}></div>
-    <div className={styles.heading}>TESTIMONIALS</div>
     <div className={styles.content}>What Our Clients Say</div>
     </div>
     <div className={styles.carousel}>
       {data.map((item, index) => (
         <div key={index} className={`${styles.slide} ${index === activeIndex ? styles.active : ''}`}>
           <div className={styles.testcard}>
-          <div className={styles.text}>{item.text}</div>
           <div className={styles.image}>
-            <img src={item.image} alt={`Image ${index + 1}`} />
+          <img src={item.image} alt={`Image ${index + 1}`} />
+          <div className={styles.text}>{item.text}</div>
+            
           </div>
         </div>
         </div>
